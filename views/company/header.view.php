b2e9425b232b0c961a2d182b574b8dd4d678d75f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-11-15 06:36:33 -0500

    subiendo proyecto

--- views/company/header.view.php
+++ views/company/header.view.php
@@ -9,7 +9,7 @@
     <meta name="theme-color" content="#162644">
     <meta name="MobileOptimized" content="width">
     <meta name="HandheldFriendly" content="true">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="shortcut icon" type="image/png" href="./img/ProgramadorFitness.png">
     <link rel="manifest" href="./manifest.json">
