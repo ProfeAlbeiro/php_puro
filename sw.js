e972765d6c7a8bdf2e6e7076f962f48745b3a578
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-11-15 08:39:15 -0500

    subiendo proyecto

--- sw.js
+++ sw.js
@@ -1,48 +1,10 @@
-// asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_php_limpio';
-const urlsToCache = [
-  '/',  
-  '/assets/landing/js/custom.js',
-  '/assets/landing/js/templatemo.js',
-  '/assets/landing/js/bootstrap.bundle.min.js',
-  '/assets/landing/css/bootstrap.min.css',
-  '/assets/landing/css/templatemo.css'
-];
+self.addEventListener("install", () => self.skipWaiting());
 
-// instalar
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-// activar
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME];
-
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-    .then(() => self.clients.claim())
-  );
-});
-
-// fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => {
-        return caches.match('/'); // mostrar al menos la página principal offline
-      });
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
