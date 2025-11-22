9fbabbd6a8e1abea398aaa606a0339d683b51dd3
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-11-22 10:50:59 -0500

    test_php_limpio

--- models/DataBase.php
+++ models/DataBase.php
@@ -23,8 +23,9 @@
                 PDO::MYSQL_ATTR_SSL_CA => 'assets/database/DigiCertGlobalRootG2.crt.pem'
             );
             $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $pdo;
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
         }
+        
 	}
 ?>
